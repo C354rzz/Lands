8f3a89a4ca9d2cf71e2eef4146f340eff1770f00
Armijo <julio.armijo.rdx@gmail.com>
2018-05-16 15:19:20 -0500

    loginViewModel

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+
+namespace Lands.ViewModels
+{
+    public class LoginViewmodel
+    {
+        #region Properties
+        public string Email
+        { get; set; }
+        public string Password
+        { get; set; }
+        public bool IsRunning
+        { get; set; }
+        public bool IsRemembered
+        { get; set; }
+        #endregion
+        #region Constructor
+        public LoginViemodel()
+        {
+            this.IsRemembered = true;
+
+        }
+        #endregion
+        #region Commands
+        public ICommand LoginCommand
+        { get; set; }
+        #endregion
+    }
+}
--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -1,7 +1,21 @@
 ﻿namespace Lands.ViewModels
 {
-
+    
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewmodel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewmodel();
+        }
+        #endregion
     }
 }
