42ea0025e11e93ac6f2fc5139eb68eddc6b7c5b7
Armijo <julio.armijo.rdx@gmail.com>
2018-05-25 00:59:38 -0500

    settings rememberme

---
+++ Lands/Lands/Helpers/Settings.cs
@@ -0,0 +1,46 @@
+﻿namespace Lands.Helpers
+{
+    using Plugin.Settings;
+    using Plugin.Settings.Abstractions;
+
+    public static class Settings
+    {
+        static ISettings AppSettings
+        {
+            get
+            {
+                return CrossSettings.Current;
+            }
+        }
+
+        const string sourceId = "SourceId";
+        const string targetId = "TargetId";
+        static readonly int intDefault = 0;
+
+        public static int SourceId
+        {
+            get
+            {
+                return AppSettings.GetValueOrDefault(sourceId, intDefault);
+            }
+            set
+            {
+                AppSettings.AddOrUpdateValue(sourceId, value);
+            }
+        }
+
+
+        public static int TargetId
+        {
+            get
+            {
+                return AppSettings.GetValueOrDefault(targetId, intDefault);
+            }
+            set
+            {
+                AppSettings.AddOrUpdateValue(targetId, value);
+            }
+        }
+    }
+
+}
